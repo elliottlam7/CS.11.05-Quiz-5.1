d20654fa3d8b6ec628f610164c7d841fe29e021b
elliottlam <elliottla2027@student.cis.edu.hk>
2024-10-30 09:57:48 +0800

    ide 5 done

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
 public class Main {
 
     /**
-     * Given two arrays of integers, write a method called combine that combines the two arrays into one larger array.
+     * Given two arrays of integers, write a method called combine that combines the two arrays into one larger array.*
      * The method combine will return a new one dimensional array of integers.
      * int[] myArray1 = {1,3,5,7,9};
      * int[] myArray2 = {2,4,6,8,10,12,14,16};
      * zip(myArray1, myArray2) → {1,3,5,7,9,2,4,6,8,10,12,14,16}
      */
     public static int[] combine(int[] array1, int[] array2) {
-
-        return null;
-
+        // makes a new array big enough to hold both arrays
+        int[] result = new int[array1.length + array2.length];
+        // copies stuff from array1 to put into new array
+        for (int i = 0; i < array1.length; i++) {
+            result[i] = array1[i];
+        }
+        // copies stuff from array2 to put into new array
+        for (int i = 0; i < array2.length; i++) {
+            result[array1.length + i] = array2[i];
+        }
+        return result;
     }
 
     /**
@@ -21,9 +29,14 @@ public class Main {
      * zip(myArray1, myArray2) → {1,2,3,4,5,6,7,8,9,10}
      */
     public static int[] zip(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] result = new int[array1.length + array2.length];
+        // make a loop so it will add elements from both arrays to the new one while alternating
+        int index = 0;
+        for (int i = 0; i < array1.length; i++) {
+            result[index++] = array1[i]; // add element from array1. i++ makes it so the next added element doesn't overlap
+            result[index++] = array2[i]; // add element from array2.
+        }
+        return result;
     }
 
     /**
@@ -34,9 +47,11 @@ public class Main {
      * product(myArray1, myArray2) → {2,12,30,56,90}
      */
     public static int[] product(int[] array1, int[] array2) {
-
-        return null;
-
+        int[] result = new int[array1.length];
+        for (int i = 0; i < array1.length; i++) {
+            result[i] = array1[i] * array2[i];
+        }
+        return result;
     }
 
     /**
@@ -56,14 +71,22 @@ public class Main {
      * capitalCount(words) → {1, 2, 2, 0}
      */
     public static int[] capitalCount(String[] words) {
-
-        return null;
-
+        int[] result = new int[words.length];
+        for (int i = 0; i < words.length; i++) {
+            result[i] = countCapitalLetters(words[i]);
+        }
+        return  result;
     }
 
     public static int countCapitalLetters(String word) {
-
-        return 0;
+        int count = 0;
+        for (int i = 0; i < word.length(); i++) {
+            char character = word.charAt(i);
+            if (Character.isAlphabetic(character) && Character.isUpperCase(character)) {
+                count++;
+            }
+        }
+        return count;
 
     }
 
